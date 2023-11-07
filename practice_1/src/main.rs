bae097431227763b1ee7de90226bc1cea2d9c8da
munachianya <munachim.anya@pau.edu.ng>
2023-11-07 22:54:14 +0100

    commit

--- practice_1/src/main.rs
+++ practice_1/src/main.rs
@@ -9,17 +9,19 @@ use greetings::french;*/
 ///Alternatively, use * to load them all
 //use greetings::*;
 ///Alternatively, load them in one line
-
-use greeting::{english, french, spanish,igbo};
+use greeting::{english, french, igbo, spanish};
 fn main() {
-    let number = [1,2,3,4,5,6];
+    let number = [1, 2, 3, 4, 5, 6];
     println!("Hello, world!");
     println!("English : {}", english::default_greeting());
     println!("Igbo : {}", igbo::default_greeting());
     println!("Spanish : {}", spanish::default_greeting());
     println!("French : {}", french::default_greeting());
-    // println!("From the lib crate : {}", hello_world_lib::greeting_from_lib());
-    how_you_hold_data_for_operations::primitive::scalar::main();    
+
+    //*calling the main function from scalar */
+    how_you_hold_data_for_operations::primitive::scalar::main();
+    
+    //*calling the main and analyze_slice function from compound */
     how_you_hold_data_for_operations::primitive::compound::analyze_slice(&number);
     how_you_hold_data_for_operations::primitive::compound::main();
 }
